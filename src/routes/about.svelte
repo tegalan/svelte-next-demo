32356f49601a1470e9bff8677c2f8b87391ea969
Sucipto <sucipto@pm.me>
2020-11-10 22:18:31 +0700

    Update

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,9 +1,9 @@
-<h1>Aboout me</h1>
-
 <script context="module">
     export const prerender = true;
 </script>
 
-<script>
-    export const prerender = true;
-</script>
+<h1>Aboout me</h1>
+
+<div>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus enim inventore non laboriosam. Fugiat assumenda saepe officia! Architecto illo officiis magnam, ut deserunt ipsum accusantium veritatis nisi modi id et!</p>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	// By default, `npm run build` will create a standard Node app.
 	// You can create optimized builds for different platforms by
 	// specifying a different adapter
-	adapter: './adapter/firebase',
-	// adapter: '@sveltejs/adapter-netlify',
+	// adapter: './adapter/firebase',
+	adapter: '@sveltejs/adapter-netlify',
 	preprocess,
 };
